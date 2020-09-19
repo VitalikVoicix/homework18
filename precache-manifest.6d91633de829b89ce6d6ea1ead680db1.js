09fe86c4e06258609199b03a608ba5899173701a
VitalikVoicix <voicix1502@gmail.com>
2020-09-20 00:17:54 +0300

    Updates

---
+++ precache-manifest.6d91633de829b89ce6d6ea1ead680db1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f911a21e80cf2abd84fd6062a986686f",
+    "url": "/homework18/index.html"
+  },
+  {
+    "revision": "7bf8dab4cecbedf03ed0",
+    "url": "/homework18/static/css/main.9c3586cb.chunk.css"
+  },
+  {
+    "revision": "ae6ed89058469651016b",
+    "url": "/homework18/static/js/2.e402500f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homework18/static/js/2.e402500f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bf8dab4cecbedf03ed0",
+    "url": "/homework18/static/js/main.67c30bde.chunk.js"
+  },
+  {
+    "revision": "540bcaa6e3c6d01d36f0",
+    "url": "/homework18/static/js/runtime-main.29cad637.js"
+  },
+  {
+    "revision": "e5d0fcfdec5d72db717894dbce3b5b7e",
+    "url": "/homework18/static/media/padlock.e5d0fcfd.png"
+  }
+]);
