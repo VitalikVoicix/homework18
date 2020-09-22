b8e9f3f7e2a4437e7f60a1412bb8a1d9242d7f92
VitalikVoicix <voicix1502@gmail.com>
2020-09-22 21:28:39 +0300

    change

--- src/Main/StyledComponent/sign.js
+++ src/Main/StyledComponent/sign.js
@@ -90,7 +90,7 @@ font-size: 20px;
 `
 const Display = styled.div`
           position: absolute;
-          top: 50%;
+          top:50%;
           left: 50%;
           transform: translate(-50%, -50%);
           z-index: 55555;
